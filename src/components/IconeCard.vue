ee1c22e365288d23e90f4fda1638e097e015e9ca
EHRSAM Lucie <101527899+Elicue@users.noreply.github.com>
2022-11-20 11:13:27 +0100

    :lipstick: Update Border

--- src/components/IconeCard.vue
+++ src/components/IconeCard.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <div class="rounded-full w-10 h-10 border-2 items-center border-border_icone_card border-solid mr-2">
+  <div class="rounded-full w-10 h-10 border-2 items-center border-border_IconeCard border-solid mr-2">
     <img class="flex mx-auto" src="../../presence.svg" alt="img">
   </div>
 </template>
